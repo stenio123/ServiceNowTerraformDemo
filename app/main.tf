93550c024fc08524d3578617d824f7832da71d46
Your Name <you@example.com>
2021-04-28 13:48:23 -0500

    returned to v2.0 to release tag

--- app/main.tf
+++ app/main.tf
@@ -1,16 +1,16 @@
 # reference to module
 /**module "db" {
   source = "../terraform_aws_db_module"
-}
+}*/
 module "db-module" {
   source  = "app.terraform.io/TFE_PoV/db-module/aws"
   version = "2.0.0"
-}*/
+}
 
 # depends_on is a fix for terraform <0.12 which reads data before references by default
 data "aws_secretsmanager_secret_version" "example" {
-  secret_id = var.aws_secret_id
-  #depends_on = [module.db-module.aws_secret_id]
+  secret_id = module.db-module.aws_secret_id
+  depends_on = [module.db-module.aws_secret_id]
 }
 
 
@@ -38,7 +38,7 @@ resource "aws_instance" "web" {
   user_data = <<-EOF
 		#! /bin/bash
         echo  ${data.aws_secretsmanager_secret_version.example.secret_string} > /home/ubuntu/secret.txt
-        echo  ${var.db_ip_addr} > /home/ubuntu/url.txt
+        echo  ${module.db-module.db_ip_addr} > /home/ubuntu/url.txt
 	EOF
   tags = {
     Name = "Stenio"
